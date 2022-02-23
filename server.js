37ce71ee504f90c77fb7bf67d2a2ed7490db0537
Nutdanai <nud.thy@gmail.com>
2022-02-23 17:25:32 +0700

    Add authentication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -8,8 +9,13 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.use(cookieParser());
+
 const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
+
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 
