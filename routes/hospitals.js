fcd5d08c94c9a98d87c5395e1a2cd2b4eb6085c1
Nutdanai <nud.thy@gmail.com>
2022-03-17 16:08:11 +0700

    SW7

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,23 +1,19 @@
 const express = require("express");
-const {
-  getHospitals,
-  getHospital,
-  createHospital,
-  deleteHospital,
-  updateHospital,
-} = require("../controllers/hospitals");
 const router = express.Router();
+const controller = require("../controllers/hospitals");
+const { protect, authorize } = require("../middlewares/auth");
+const appointmentRouter = require("./appointments");
 
-const { protect, authorize } = require("../middleware/auth");
-
+router.use("/:hospitalId/appointments", appointmentRouter);
 router
   .route("/")
-  .get(getHospitals)
-  .post(protect, authorize("admin"), createHospital);
+  .get(controller.getHospitals)
+  .post(protect, authorize("admin"), controller.createHospital);
+
 router
   .route("/:id")
-  .get(getHospital)
-  .put(protect, authorize("admin"), updateHospital)
-  .delete(protect, authorize("admin"), deleteHospital);
+  .get(controller.getHospital)
+  .put(protect, authorize("admin"), controller.updateHospital)
+  .delete(protect, authorize("admin"), controller.deleteHospital);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,34 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
-const app = express();
-app.use(express.json());
 
+const app = express();
+
+//add cookie parser
 app.use(cookieParser());
 
-const hospitals = require("./routes/hospitals");
+const hospitals = require("./routes/hospitals.js");
+const register = require("./routes/auth");
 const auth = require("./routes/auth");
+const appointments = require("./routes/appointments");
+app.use(express.json());
 
 app.use("/api/v1/hospitals", hospitals);
-app.use("/api/v1/auth", auth);
+app.use("/api/v1/auth", register);
+app.use("/api/v1/appointments", appointments);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(
-  PORT,
-  console.log(
-    "Server running in ",
-    process.env.NODE_ENV,
-    " mode on port ",
-    PORT
-  )
-);
+const server = app.listen(PORT, () => {
+  console.log(`server running in ${process.env.NODE_ENV} mode on ${PORT}`);
+});
 
 process.on("unhandledRejection", (err, Promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error : ${err.message}`);
   server.close(() => process.exit(1));
 });
